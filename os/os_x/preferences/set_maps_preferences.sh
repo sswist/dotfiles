b8d6d6ddc77c8113d3f0bb203db70079f0fba32c
sswist <sswist@gmail.com>
2016-07-06 12:30:51 -0400

    delete shell folder, change maps and term prefs

--- os/os_x/preferences/set_maps_preferences.sh
+++ os/os_x/preferences/set_maps_preferences.sh
@@ -10,7 +10,7 @@ set_preferences() {
     execute 'defaults write com.apple.Maps LastClosedWindowViewOptions "{
                 localizeLabels = 1;   // show labels in English
                 mapType = 11;         // show hybrid map
-                trafficEnabled = 0;   // do not show traffic
+                trafficEnabled = 1;   // show traffic
              }"' \
         'Set view options'
 
