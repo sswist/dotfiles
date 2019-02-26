f769e1bc14bd09f822158528b00a2355a4c2c005
sswist <sswist@gmail.com>
2019-02-26 09:02:15 -0500

    Added an app

--- os/os_x/installs/main.sh
+++ os/os_x/installs/main.sh
@@ -66,7 +66,7 @@ main() {
   #  brew_install 'Opera' 'opera' 'caskroom/cask' 'cask'
   #  brew_install 'Opera Beta' 'opera-beta' 'caskroom/versions' 'cask'
   #  brew_install 'Opera Developer' 'opera-developer' 'caskroom/versions' 'cask'
-  #  brew_install 'Parallels Desktop' 'parallels-desktop' 'caskroom/cask' 'cask'
+    brew_install 'Parallels Desktop' 'parallels' 'caskroom/cask' 'cask'
   #  brew_install 'Reaper' 'reaper' 'caskroom/cask' 'cask'
   #  brew_install 'Scrivener' 'scrivener' 'caskroom/cask' 'cask'
   #  brew_install 'Slack' 'slack' 'caskroom/cask' 'cask'
